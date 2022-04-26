8451c0bea48054dd7e3f7612417ae6d23316fbcf
James Ware <jazz642@gmail.com>
2022-04-26 17:28:17 -0400

    Did more re-factoring, broke out program logic into much smaller functions. Still have some cleanup and functionality to add

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'hashie'
+require 'pry-nav'
 
 class Calculator
   class << self
@@ -11,12 +12,49 @@ class Calculator
       'apple' => Hashie::Mash.new({ price: 0.89 })
     }.freeze
 
-    def build_purchases_breakdown(purchases)
-      grocery_report = {}
+    def build_grocery_purchase_data(purchases)
+      grocery_report = Hashie::Mash.new(total_purchase_price: 0, total_savings: 0)
       purchases.each do |item|
-        grocery_report[item] ? grocery_report[item] += 1 : grocery_report[item] = 1
+        update_line_item(grocery_report, item)
       end
+      add_summary_data(grocery_report)
       grocery_report
+    end
+
+    private
+    
+    def add_summary_data(grocery_report)
+      # do total, sales total should be here already?
+    end
+
+    def update_savings_total(grocery_report, discount)
+      grocery_report['total_savings'] += discount
+    end
+
+    def adjust_for_sales_price(grocery_report, item)
+      # I am assuming based on the stated requirements that the sale's price can only apply once and not for each sales quantity
+      # I can simply change the total price once if the sales quantity is hit given the assumption that a sales price can only occur once per item
+      return unless grocery_report[item].quantity == GROCERIES[item].sale_quantity
+
+      discount = grocery_report[item].line_item_total_price - GROCERIES[item].sale_price
+      grocery_report[item].line_item_total_price = GROCERIES[item].sale_price
+      update_savings_total(grocery_report, discount)
+    end
+
+    def update_line_item_total_price(grocery_report, item)
+      grocery_report[item].line_item_total_price += GROCERIES[item].price
+      adjust_for_sales_price(grocery_report, item)
+    end
+
+    def update_line_item_quantity(grocery_report, item)
+      grocery_report[item].quantity += 1
+    end
+
+    def update_line_item(grocery_report, item)
+      grocery_report[item] = Hashie::Mash.new(quantity: 0, line_item_total_price: 0) unless grocery_report[item]
+
+      update_line_item_quantity(grocery_report, item)
+      update_line_item_total_price(grocery_report, item)
     end
   end
 end
--- main.rb
+++ main.rb
@@ -2,23 +2,10 @@ require_relative 'lib/calculator'
 require_relative 'lib/receipt_builder'
 require 'pry-nav'
 
-#puts Calculator::GROCERIES['milk'].price
-
-
-
-def calculate_item_grocery_report(groceries)
-  puts groceries
-  #Calculator::GROCERIES[item_key].price * quantity
-end
-
 puts "Please enter all the items purchased separated by a comma"
 #purchases = gets.chomp.delete(' ').split(',')
 purchases = "milk,milk, bread,banana,bread,bread,bread,milk,apple".delete(' ').split(',')
 
-grocery_report = Calculator.build_purchases_breakdown(purchases)
-
-
-
-#calculate_item_grocery_report(grocery_report)
+grocery_report = Calculator.build_grocery_purchase_data(purchases)
 
 ReceiptBuilder.print_receipt(grocery_report)
